e56c1e0f797da816a1d8fe8c043d02c8c83beabe
Nikodem.Tokarski <nikodem.tokarski@comarch.pl>
2015-10-22 14:27:29 +0200

    start

---
+++ src/main/java/Test/BasicView.java
@@ -0,0 +1,33 @@
+package Test;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import Test.Car;
+import Test.CarService;
+ 
+@ManagedBean(name="dtBasicView")
+@ViewScoped
+public class BasicView implements Serializable {
+     
+    private List<Car> cars;
+     
+    @ManagedProperty("#{carService}")
+    private CarService service;
+ 
+    @PostConstruct
+    public void init() {
+        cars = service.createCars(50);
+    }
+     
+    public List<Car> getCars() {
+        return cars;
+    }
+ 
+    public void setService(CarService service) {
+        this.service = service;
+    }
+}
